1971ef2d9302b4374513cd3c35dee74ac143cc35
baiwfg2 <baiwfg2@hotmail.com>
2015-05-18 15:06:01 +0800

    the first commit

---
+++ lib/common.py
@@ -0,0 +1,74 @@
+import uuid
+import random
+import json
+
+def is_hwid_existed(id):
+	# file must exist first
+	f = open('cfg/gw_json.cfg','r')
+	strdata = f.read()
+	dic = json.loads(strdata)
+
+	if dic['hwid'] == id:
+		return True
+	else:
+		return False
+
+def is_mwid_existed(hwid):
+	f = open('cfg/gw_json.cfg','r')
+	strdata = f.read()
+	dic = json.loads(strdata)
+
+	if dic['hwid'] == hwid and dic['mwid'] != '':
+		return dic['mwid']
+	else:
+		return ''
+
+def is_updated():
+	f = open('cfg/gw_json.cfg','r')
+	strdata = f.read()
+	dic = json.loads(strdata)
+
+	if dic['updated'] == '1':
+		return True
+	else:
+		return False
+
+def wr_settings(data,devid,flag):
+	fr = open('cfg/gw_json.cfg','r')
+	dic = json.loads(fr.read())
+	fr.close()
+
+	fw = open('cfg/gw_json.cfg','w')
+	if flag == 0:
+		# write hwid		
+		dic['hwid'] = data	
+
+	elif flag == 1:
+		# write mwid
+		dic['mwid'] = data	
+
+	elif flag == 2:
+		# write updated flag
+		dic['updated'] = data	
+		
+	else:
+		print 'illegal id'
+
+	fw.write(json.dumps(dic))
+	fw.close()
+
+def get_mac_addr():
+	node = uuid.getnode()
+	return uuid.UUID(int=node).hex[-12:]
+	
+def sensor_data():
+	return random.randint(1,100)
+
+def rd_prop(filename):
+	f = open(filename,'r')
+	return f.read()
+	
+
+def clear_file(filename):
+	f = open(filename,'w')
+	f.write('')
---
+++ lib/init.py
@@ -0,0 +1,108 @@
+import os
+
+hostandport=''
+url_registerHW=''
+url_registerMW=''
+url_deleteGW=''
+url_updateMW=''
+
+url_delDevice=''
+url_addDevice=''
+url_addRes=''
+url_delRes=''
+
+url_uploadData=''
+url_sensorData=''
+
+url_hb=''
+url_camera=''
+url_sensor=''
+url_control=''
+url_config=''
+url_watchdog=''
+
+def rd_local_cfg():
+	global hostandport
+	global url_registerHW
+	global url_registerMW
+	global url_deleteGW
+	global url_updateMW
+	
+	global url_delDevice
+	global url_addDevice
+	global url_addRes
+	global url_delRes
+	
+	global url_uploadData
+	global url_sensorData
+	global url_hb
+	global url_control
+	global url_camera
+	
+	for line in open("cfg/local.cfg"):
+		l = line.strip().split('=')
+		if l[0] == 'serverIP':
+			host = l[1]
+		elif l[0] == 'serverPort':
+			port = l[1]
+		elif l[0] == 'udpPort':
+			pass
+		elif l[0] == 'registerHWUrl':
+			url_registerHW = l[1]
+		elif l[0] == 'registerMWUrl':
+			url_registerMW = l[1]
+		elif l[0] == 'hBUrl':
+			url_hb = l[1]
+		elif l[0] == 'cameraUrl':
+			url_camera = l[1]
+		elif l[0] == 'sensorUrl':
+			url_sensor = l[1]
+		elif l[0] == 'controlUrl':
+			url_control = l[1]
+		elif l[0] == 'configUrl':
+			url_config = l[1]
+		elif l[0] == 'watchDogUrl':
+			url_watchdog = l[1]
+		elif l[0] == 'deleteUrl':
+			url_deleteGW = l[1]
+		elif l[0] == 'updateMWUrl':
+			url_updateMW = l[1]
+		elif l[0] == 'delDeviceUrl':
+			url_delDevice = l[1]
+		elif l[0] == 'addDeviceUrl':
+			url_addDevice = l[1]
+		elif l[0] == 'addResUrl':
+			url_addRes = l[1]
+		elif l[0] == 'delResUrl':
+			url_delRes = l[1]
+		elif l[0] == 'uploadUrl':
+			url_uploadData = l[1]
+		elif l[0] == 'sensorDataUrl':
+			url_sensorData = l[1]
+			
+	hostandport = 'http://' + host + ':' + port
+	url_registerHW = hostandport + url_registerHW
+	url_registerMW = hostandport + url_registerMW
+	url_deleteGW = hostandport + url_deleteGW
+	url_updateMW = hostandport + url_updateMW
+	
+	url_delDevice = hostandport + url_delDevice
+	url_addDevice = hostandport + url_addDevice
+	url_addRes = hostandport + url_addRes
+	url_delRes = hostandport + url_delRes
+	
+	url_uploadData = hostandport + url_uploadData
+	url_sensorData = hostandport + url_sensorData
+	
+	url_hb = hostandport + url_hb
+	url_camera =  hostandport + url_camera
+	url_sensor = hostandport + url_sensor
+	
+	url_control =  hostandport + url_control
+	url_config = hostandport + url_config
+	url_watchdog = hostandport + url_watchdog
+
+def rd_main_cfg():
+	f = open('main.cfg','r')
+	return f.read()
+	
